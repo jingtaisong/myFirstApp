c15d1435431fcf2f96b8bffcf04eeb9df542c8be
Taisong Jing <taisong@dyn-160-39-63-174.dyn.columbia.edu>
2018-06-13 20:49:21 -0400

    add a test dummy reducer

--- client/lib/Reducer/index.js
+++ client/lib/Reducer/index.js
@@ -1,6 +1,15 @@
 import { combineReducers } from 'redux';
+import Immutable from 'immutable';
+
+const testReducer = (state = Immutable.Map(), action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
 
 const reducers = combineReducers({
+    test: testReducer,
 })
 
 export default reducers;
